828812f0e9b95b0314ecfd408a3dc027c55259dd
Jimmy Loyola <jimmy.loyola@topcontrol.it>
2026-07-16 17:10:52 +0200

    004 solitario

---
+++ examples/core/exercises/004_shapes_click_rect_point_collision_solitario.c
@@ -0,0 +1,27 @@
+#include "raylib.h"
+
+int main(void)
+{
+   InitWindow(800, 600, "003_shapes");
+   SetTargetFPS(60);
+   Color RectColor = RED;
+   Rectangle rect = { 100, 100, 100, 100 };
+   while (!WindowShouldClose())
+
+  {
+   Vector2 mouse_point = GetMousePosition();
+   if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+   {
+    if (CheckCollisionPointRec(mouse_point, rect)){
+    if (ColorIsEqual(RectColor, RED)) {
+	    RectColor = BLUE;
+    } else {
+	RectColor = RED;
+    }
+    }
+   }
+   BeginDrawing();
+   DrawRectangle(rect, RectColor);
+   EndDrawing();
+  }
+}
