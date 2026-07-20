b96609b945348a3ccef8ebee3bc45ecc768775db
Jimmy Loyola <jimmy.loyola@topcontrol.it>
2026-07-20 13:03:21 +0200

    add exerxises 6

--- examples/core/exercises/006_clang.c
+++ examples/core/exercises/006_clang.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+char membyte[50];
+
+char* saluta(char* nome) {
+  sprintf(membyte, "Ciao amico so come ti chiami %s", nome);
+  return membyte;
+}
+
 int main(int argc, char *argv[]) {
   printf("valore di argc: %d\n", argc);
   printf("valore di argv 0: %s\n", argv[0]);
   printf("valore di argv 1 %s\n", argv[1]);
+  char* testo_di_saluto = saluta(argv[1]);
+  printf("%s\n", testo_di_saluto);
   return 0;
 }
