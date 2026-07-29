409a54d27712a244704356331561010a987b988d
Jimmy Loyola <jimmy.loyola@topcontrol.it>
2026-07-29 21:32:30 +0200

    fillrect

--- examples/core/exercises/024_fillrect.c
+++ examples/core/exercises/024_fillrect.c
@@ -1,26 +1,33 @@
 #include "raylib.h"
 
-void fillrect(int x, int y, int width, int height, Color fillcolor) {
+void fillrect(const int x, const int y, const int width, const int height, Color fillcolor) {
 
-while(x < width) {
-while(y < height) {
-DrawPixel(x, y, fillcolor);
-y = y + 1;
+  int cx = 0;
+  int cy = 0;
+  while(cx < width) {
+    while(cy < height) {
+    DrawPixel(x + cx, y + cy, fillcolor);
+    cy = cy + 1;
+    }
+    cy = 0;
+    cx = cx + 1;
+  }
 }
-y = 250;
-x = x + 1;
-}
-}
-
 int main(void) {
-InitWindow(1000, 1000, "fillrect");
-SetTargetFPS(60);
-while(!WindowShouldClose()) {
-ClearBackground(BLACK);
-BeginDrawing();
-fillrect(250, 250, 750, 750, RED);
-EndDrawing();
+  InitWindow(1000, 1000, "fillrect");
+  SetTargetFPS(60);
+  while(!WindowShouldClose()) {
+    ClearBackground(BLACK);
+    BeginDrawing();
+    fillrect(0, 0, 500, 500, RED);
+    fillrect(500, 0, 500, 500, BLUE);
+    fillrect(0, 500, 500, 500, YELLOW);
+    fillrect(500, 500, 500, 500, GREEN);
+    fillrect(250, 250, 500, 500, WHITE);
+    EndDrawing();
 
-} 
-return 0;
-}
+  } 
+  return 0;
+  }
+
+
